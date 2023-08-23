6621f4607f347798ccd355799b3db0d5cd52b419
yunus <yunus@YNSPC>
2023-08-23 07:39:39 +0300

    Proje dosyası ekle.

---
+++ Form1.Designer.cs
@@ -0,0 +1,244 @@
+﻿namespace ucaklar02
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.ucak = new System.Windows.Forms.PictureBox();
+            this.mermi = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.ucak1 = new System.Windows.Forms.PictureBox();
+            this.mermi1 = new System.Windows.Forms.PictureBox();
+            this.lbCan = new System.Windows.Forms.Label();
+            this.lbSure = new System.Windows.Forms.Label();
+            this.ucak2 = new System.Windows.Forms.PictureBox();
+            this.mermi2 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lbPuan = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.ucak)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.mermi)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ucak1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.mermi1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ucak2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.mermi2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // ucak
+            // 
+            this.ucak.Image = ((System.Drawing.Image)(resources.GetObject("ucak.Image")));
+            this.ucak.Location = new System.Drawing.Point(268, 568);
+            this.ucak.Name = "ucak";
+            this.ucak.Size = new System.Drawing.Size(87, 90);
+            this.ucak.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.ucak.TabIndex = 0;
+            this.ucak.TabStop = false;
+            // 
+            // mermi
+            // 
+            this.mermi.Image = ((System.Drawing.Image)(resources.GetObject("mermi.Image")));
+            this.mermi.Location = new System.Drawing.Point(291, 522);
+            this.mermi.Name = "mermi";
+            this.mermi.Size = new System.Drawing.Size(40, 40);
+            this.mermi.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.mermi.TabIndex = 1;
+            this.mermi.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // ucak1
+            // 
+            this.ucak1.BackColor = System.Drawing.Color.Transparent;
+            this.ucak1.Image = ((System.Drawing.Image)(resources.GetObject("ucak1.Image")));
+            this.ucak1.Location = new System.Drawing.Point(75, 95);
+            this.ucak1.Name = "ucak1";
+            this.ucak1.Size = new System.Drawing.Size(87, 90);
+            this.ucak1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.ucak1.TabIndex = 2;
+            this.ucak1.TabStop = false;
+            // 
+            // mermi1
+            // 
+            this.mermi1.Image = ((System.Drawing.Image)(resources.GetObject("mermi1.Image")));
+            this.mermi1.Location = new System.Drawing.Point(95, 191);
+            this.mermi1.Name = "mermi1";
+            this.mermi1.Size = new System.Drawing.Size(40, 40);
+            this.mermi1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.mermi1.TabIndex = 3;
+            this.mermi1.TabStop = false;
+            // 
+            // lbCan
+            // 
+            this.lbCan.AutoSize = true;
+            this.lbCan.BackColor = System.Drawing.Color.DodgerBlue;
+            this.lbCan.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lbCan.ForeColor = System.Drawing.Color.White;
+            this.lbCan.Location = new System.Drawing.Point(610, 12);
+            this.lbCan.Name = "lbCan";
+            this.lbCan.Size = new System.Drawing.Size(43, 29);
+            this.lbCan.TabIndex = 4;
+            this.lbCan.Text = "10";
+            // 
+            // lbSure
+            // 
+            this.lbSure.AutoSize = true;
+            this.lbSure.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lbSure.ForeColor = System.Drawing.Color.White;
+            this.lbSure.Location = new System.Drawing.Point(478, 9);
+            this.lbSure.Name = "lbSure";
+            this.lbSure.Size = new System.Drawing.Size(28, 29);
+            this.lbSure.TabIndex = 5;
+            this.lbSure.Text = "0";
+            this.lbSure.Visible = false;
+            // 
+            // ucak2
+            // 
+            this.ucak2.BackColor = System.Drawing.Color.Transparent;
+            this.ucak2.Image = ((System.Drawing.Image)(resources.GetObject("ucak2.Image")));
+            this.ucak2.Location = new System.Drawing.Point(446, 95);
+            this.ucak2.Name = "ucak2";
+            this.ucak2.Size = new System.Drawing.Size(87, 90);
+            this.ucak2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.ucak2.TabIndex = 6;
+            this.ucak2.TabStop = false;
+            // 
+            // mermi2
+            // 
+            this.mermi2.Image = ((System.Drawing.Image)(resources.GetObject("mermi2.Image")));
+            this.mermi2.Location = new System.Drawing.Point(466, 191);
+            this.mermi2.Name = "mermi2";
+            this.mermi2.Size = new System.Drawing.Size(40, 40);
+            this.mermi2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.mermi2.TabIndex = 7;
+            this.mermi2.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.DodgerBlue;
+            this.label1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(682, 50);
+            this.label1.TabIndex = 10;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.DodgerBlue;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(526, 12);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(78, 29);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "Can :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.DodgerBlue;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(12, 11);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(92, 29);
+            this.label3.TabIndex = 13;
+            this.label3.Text = "Puan :";
+            // 
+            // lbPuan
+            // 
+            this.lbPuan.AutoSize = true;
+            this.lbPuan.BackColor = System.Drawing.Color.DodgerBlue;
+            this.lbPuan.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lbPuan.ForeColor = System.Drawing.Color.White;
+            this.lbPuan.Location = new System.Drawing.Point(85, 12);
+            this.lbPuan.Name = "lbPuan";
+            this.lbPuan.Size = new System.Drawing.Size(43, 29);
+            this.lbPuan.TabIndex = 12;
+            this.lbPuan.Text = "10";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DeepSkyBlue;
+            this.ClientSize = new System.Drawing.Size(682, 708);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.lbPuan);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lbSure);
+            this.Controls.Add(this.lbCan);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.ucak);
+            this.Controls.Add(this.ucak2);
+            this.Controls.Add(this.mermi2);
+            this.Controls.Add(this.ucak1);
+            this.Controls.Add(this.mermi1);
+            this.Controls.Add(this.mermi);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "BATTLE OF PLANES";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.ucak)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.mermi)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ucak1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.mermi1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ucak2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.mermi2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox ucak;
+        private System.Windows.Forms.PictureBox mermi;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.PictureBox ucak1;
+        private System.Windows.Forms.PictureBox mermi1;
+        private System.Windows.Forms.Label lbCan;
+        private System.Windows.Forms.Label lbSure;
+        private System.Windows.Forms.PictureBox ucak2;
+        private System.Windows.Forms.PictureBox mermi2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lbPuan;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,240 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ucaklar02
+{
+    public partial class Form1 : Form
+    {
+        Random random = new Random();
+        bool ates = false;
+        bool ates1 = false;
+        bool ates2 = false;
+      
+        int mermiHiz = 20;
+        int mermiHiz1 = 15;
+        int mermiHiz2 = 15;
+    
+        int ucakHiz = 8;
+        int ucakHiz2 = 8;
+
+
+        int can = 10;
+        int sure = 0;
+        int puan = 0;
+        public Form1()
+        {
+            InitializeComponent();
+            baslama();
+
+        }
+
+        private void baslama()
+        {
+            ucak1.Top = -(random.Next(1, 200));
+            ucak1.Left=random.Next(1, 600);
+            mermi1.Top = ucak1.Top + 40;
+            mermi1.Left = ucak1.Left + 25;
+            ucak2.Top = -(random.Next(1, 200));
+            ucak2.Left = random.Next(1, 600);
+            mermi2.Top = ucak2.Top + 40;
+            mermi2.Left = ucak2.Left + 25;
+            sure = 0;
+            puan = 0;
+            can = 10;
+
+        }
+        
+        private void OyunSonu()
+        {
+            timer1.Enabled = false;
+            MessageBox.Show(puan + " Puan Kazandınız.", "OYUN SONU", MessageBoxButtons.OK, MessageBoxIcon.Stop);
+           
+           
+            baslama();
+        }
+
+        private void atesleme1()
+        {
+            if (ates1 == false)
+            {
+                mermi1.Visible = true;
+                mermiHiz1 = 12;
+                mermi1.Left = ucak1.Left + 25;
+                mermi1.Top = ucak1.Top + 40; 
+            }
+        }
+        private void atesleme2()
+        {
+            if (ates2 == false)
+            {
+                mermi2.Visible = true;
+                mermiHiz2 = 12;
+                mermi2.Left = ucak2.Left + 25;
+                mermi2.Top = ucak2.Top + 40;
+            }
+        }
+
+        private void vurma()
+        {
+            if (mermi.Bounds.IntersectsWith(ucak1.Bounds))
+            {
+                puan += 1;
+                lbPuan.Text = puan.ToString();
+                ucak1.Top = -50;
+                int ranP = random.Next(1, 300);
+                ucak1.Left = ranP;
+                ates = false;
+                mermiHiz = 0;
+                mermi.Top = ucak.Top+40;
+                mermi.Left = ucak.Left+25;
+            }
+            if (mermi.Bounds.IntersectsWith(ucak2.Bounds))
+            {
+                puan += 1;
+                lbPuan.Text = puan.ToString();
+                ucak2.Top = -50;
+                int ranP = random.Next(1, 300);
+                ucak2.Left = ranP;
+                ates = false;
+                mermiHiz = 0;
+                mermi.Top = ucak.Top + 40;
+                mermi.Left = ucak.Left + 25;
+            }
+        }
+        private void vurulma()
+        {
+            if (mermi1.Bounds.IntersectsWith(ucak.Bounds))
+            {
+                can -= 1;
+                lbCan.Text = can.ToString();
+                if (can == 0) { OyunSonu(); }
+                ucak.Left = random.Next(1, 500);          
+            }
+            else if (mermi2.Bounds.IntersectsWith(ucak.Bounds))
+            {
+                can -= 1;
+                lbCan.Text = can.ToString();
+                if (can == 0) { OyunSonu(); }
+                ucak.Left = random.Next(1, 500);       
+            }
+            else if (ucak1.Bounds.IntersectsWith(ucak.Bounds))
+            {
+                can -= 1;
+                lbCan.Text = can.ToString();
+                puan += 1;
+                lbPuan.Text = puan.ToString();
+                if (can == 0) { OyunSonu(); }
+                ucak.Left = random.Next(1, 500);
+                ucak1.Top = -50;
+                int ranP = random.Next(1, 300);
+                ucak1.Left = ranP;
+            }
+            else if (ucak2.Bounds.IntersectsWith(ucak.Bounds))
+            {
+                can -= 1;
+                lbCan.Text = can.ToString();
+                puan += 1;
+                lbPuan.Text = puan.ToString();
+                if (can == 0) { OyunSonu(); }
+                ucak.Left = random.Next(1, 500);
+                ucak2.Top = -50;
+                int ranP = random.Next(1, 300);
+                ucak2.Left = ranP;
+            }
+        }
+
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode == Keys.Enter) { timer1.Start(); }
+            int x=ucak.Location.X;
+            int y=ucak.Location.Y;
+            if (e.KeyCode==Keys.Left && x > 0) { x=x-20;}//sol ok a basıldığında sola doğru 20 piksel git
+            if (e.KeyCode == Keys.Right && x <600) { x = x + 20; }//sağ ok a basıldığında sağa doğru 20 piksel git
+            ucak.Location=new Point(x, y); // uçak yeni lokasyonu tanımlanıyor
+            if (e.KeyCode == Keys.Space) //ana uçak ateş olayları
+            {
+                if (ates == false)
+                {
+                    mermi.Visible = true;
+                    mermiHiz = 12;
+                    mermi.Left = ucak.Left + 25;
+                    mermi.Top = ucak.Top-40;
+                }
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            timer1.Stop();
+            this.Width = 700;//formun genişliği
+            this.Height = 700; //formun yüksekliği
+            mermi.Visible = false;
+            puan = 0;
+            lbPuan.Text = puan.ToString();
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            sure += 1;
+            lbSure.Text=sure.ToString();
+
+            //ana uçak ateş 
+            mermi.Top -= mermiHiz;
+           
+            if (ates && mermi.Top < 0)
+            {
+                ates = false;
+                mermiHiz = 0;
+                mermi.Top = ucak.Left + 25;
+                mermi.Left = ucak.Top + 40;
+
+            }
+            
+            //ucaklar hareket
+            ucak1.Top += ucakHiz;
+            if (ucak1.Top >= 600)
+            {
+                ucakHiz = 0;
+                can -= 1;
+                lbCan.Text = can.ToString();
+                if (can == 0) { OyunSonu(); }
+                //uçak1 yeniden baştan konumlandırılıyor
+                ucak1.Top = -(random.Next(1, 500));
+                ucak1.Left = random.Next(1, 600);
+                ucakHiz = 8;
+            }
+            ucak2.Top += ucakHiz;
+            if (ucak2.Top >= 600)
+            {
+                ucakHiz2 = 0;
+                can -= 1;
+                lbCan.Text = can.ToString();
+                if (can == 0) { OyunSonu(); }
+                //uçak2 yeniden baştan konumlandırılıyor
+                ucak2.Top = -(random.Next(1, 500));
+                ucak2.Left = random.Next(1, 600);
+                ucakHiz2 = 8;
+            }
+
+            //ucak1ardan ateş
+            int atesSure1=
+            mermi1.Top += mermiHiz1;
+            if (sure % random.Next(80, 200) == 0) { atesleme1(); vurulma(); }
+            mermi2.Top += mermiHiz2;
+            if (sure % random.Next(60, 200) == 0) { atesleme2(); vurulma(); }
+
+            vurma();
+            vurulma();
+        }
+    }
+}
